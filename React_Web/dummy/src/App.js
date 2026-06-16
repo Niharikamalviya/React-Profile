3062794bea8b5e4e53ad7c90d705a392a8715fcf
Niharikamalviya <neharikamalviya05@gmail.com>
2026-06-16 11:33:42 +0530

    top-courses

--- React_Web/dummy/src/App.js
+++ React_Web/dummy/src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import { apiUrl, filterData } from "./data";
 import './App.css';
 import { useState, useEffect } from 'react';
+import { toast } from "react-toastify";
 
 
-function App() {
+const App = () => {
+
+  const [courses, setCourses] = useState(null);
 
   useEffect(() => {
-    console.log("UI rendering DOne");
-  });
-  const [text, setText] = useState('');
-  function changeHandler(event) {
-    console.log(text);
-    setText(event.target.value);
-  }
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        const output = await res.json();
+        // save data
+        setCourses(output.data);
+      }
+      catch (error) {
+        toast.error("somthing went wrong");
+
+      }
+    }
+
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <input type="text" onChange={changeHandler}></input>
+      <Navbar />
 
+      <Filter filterData={filterData} />
+
+      <Cards courses={courses} />
     </div>
   );
 }
